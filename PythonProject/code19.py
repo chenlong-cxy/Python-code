8604911ec11178ab7fd7078fad229a36b0d749b2
chenlong-xcy <chenlonghyl@163.com>
2022-09-13 09:34:52 +0800

    Python语法学习

---
+++ PythonProject/code19.py
@@ -0,0 +1,18 @@
+# choice = input("输入1表示愿意认真学习，输入2表示躺平摆烂: ")
+#
+# # 注意input得到的是字符串
+# if choice == '1':
+#     print("你能找到好工作")
+# elif choice == '2':
+#     print("你可能毕业就失业了")
+# else:
+#     print("你的输入有误！")
+
+a = input('请输入第一个数: ')
+b = input('请输入第二个数: ')
+
+if a == '1':
+    if b == '2':
+        print('aaa')
+    print('bbb')
+print('ccc')
---
+++ PythonProject/code20.py
@@ -0,0 +1,45 @@
+# 输入一个整数，判断是否是奇数
+# a = input('请输入一个整数: ')
+# a = int(a)
+# a = int(input('请输入一个整数: '))
+#
+# # C++和Java中负数模2等于-1，但Python中可以
+# if a % 2 == 1:
+#     print('这是一个奇数')
+# else:
+#     print('这是一个偶数')
+
+# 输入一个整数，判断是正数还是负数
+# a = int(input('请输入一个整数: '))
+#
+# if a > 0:
+#     print('正数')
+# elif a < 0:
+#     print('负数')
+# else:
+#     print('0')
+
+# 判定年份是否是闰年
+# year = int(input('请输入一个年份: '))
+
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print('闰年')
+# else:
+#     print('平年')
+
+# if year % 100 == 0: # 世纪闰年
+#     if year % 400 == 0:
+#         print('闰年')
+#     else:
+#         print('平年')
+# else: # 普通闰年
+#     if year % 4 == 0:
+#         print('闰年')
+#     else:
+#         print('平年')
+
+a = int(input('请输入一个数: '))
+if a != 1:
+    pass
+else:
+    print('hello')
