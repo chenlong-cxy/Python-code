e0edee0577540331c3cc98537a699a28053b9798
chenlong-xcy <chenlonghyl@163.com>
2022-11-04 22:59:08 +0800

    Python语法学习

--- PythonProject/projectPro3.py
+++ PythonProject/projectPro3.py
@@ -2,30 +2,25 @@
 import os
 import sys
 
-# 使用这个全局变量来管理所有的学生信息
-students = []
+# 使用字典来存储所有的学生信息
+students = {}
 
 
 def save():
-    """
-    用于存档
-    """
     with open('record.txt', 'w', encoding='utf8') as f:
-        for s in students:
-            f.write(f"{s['studentId']}\t{s['name']}\t{s['gender']}\t{s['className']}\n")
+        for sId in students:
+            sInfo = students[sId]
+            f.write(f"{sId}\t{sInfo['name']}\t{sInfo['gender']}\t{sInfo['className']}\n")
         print(f'[存档完毕] 共存储了 {len(students)} 条记录!')
 
 
 def load():
-    """
-    用于读档
-    """
     # 如果存档文件不存在则直接跳过读档流程(避免导致异常)
     if not os.path.exists('record.txt'):
         return
     # 读档的时候要保证把旧的数据清理干净
     global students
-    students = []
+    students = {}
     with open('record.txt', 'r', encoding='utf8') as f:
         for line in f:
             # 针对这一行数据, 按照 \t 进行切分操作
@@ -35,13 +30,13 @@ def load():
             if len(tokens) != 4:
                 print(f'当前行格式存在问题! line = {line}')
                 continue
-            student = {
-                'studentId': tokens[0],
+            studentId = tokens[0]
+            studentInfo = {
                 'name': tokens[1],
                 'gender': tokens[2],
                 'className': tokens[3]
             }
-            students.append(student)
+            students[studentId] = studentInfo
         print(f'[读档完毕] 共读取了 {len(students)} 条记录!')
 
 
@@ -65,14 +60,13 @@ def insert():
         return
     className = input('请输入学生的班级: ')
     # 使用一个字典将上述的信息给聚合起来
-    student = {
-        'studentId': studentId,
+    studentInfo = {
         'name': name,
         'gender': gender,
         'className': className
     }
     global students
-    students.append(student)
+    students[studentId] = studentInfo
     # 新增保存操作
     save()
     print('[新增学生] 完毕!')
@@ -81,8 +75,9 @@ def insert():
 def show():
     # 遍历全局遍历students列表, 把所有学生的信息打印出来
     print('[显示学生] 开始!')
-    for s in students:
-        print(f"[{s['studentId']}\t{s['name']}\t{s['gender']}\t{s['className']}]")
+    for sId in students:
+        sInfo = students.get(sId)
+        print(f"{sId}\t{sInfo['name']}\t{sInfo['gender']}\t{sInfo['className']}")
     print(f'[显示学生] 完毕! 共显示了 {len(students)} 条数据!')
 
 
@@ -90,9 +85,9 @@ def find():
     print('[查找学生] 开始!')
     name = input('请输入要查找的同学姓名: ')
     count = 0
-    for s in students:
-        if s['name'] == name:
-            print(f"[{s['studentId']}\t{s['name']}\t{s['gender']}\t{s['className']}]")
+    for sId in students:
+        if sId['name'] == name:
+            print(f"[{sId}\t{sId['name']}\t{sId['gender']}\t{sId['className']}]")
             count += 1
     print(f'[查找学生] 完毕! 共找到了 {count} 个匹配的同学')
 
@@ -101,10 +96,11 @@ def delete():
     print('[删除学生] 开始!')
     studentId = input('请输入要删除的学生学号: ')
     count = 0
-    for s in students:
-        if s['studentId'] == studentId:
-            print(f"删除 {s['name']} 同学的信息!")
-            students.remove(s)
+    for sId in students:
+        if sId == studentId:
+            sInfo = students.get(sId)
+            print(f"删除 {sInfo['name']} 同学的信息!")
+            students.pop(sId)
     # 新增保存操作
     save()
     print('[删除学生] 完毕!')
